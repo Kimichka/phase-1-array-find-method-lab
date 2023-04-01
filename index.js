ce308edf83cd8eb58aa84922668dfbd43562f9af
Mohammad Reza Enteshari <reza.ent@hotmail.com>
2023-04-01 16:29:44 -0400

    Lab Complete

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // code your solution here
+function superbowlWin(record) {
+const winObjects = record.find(obj => obj.result === "W");
+if (winObjects) {return winObjects.year;} 
+else {return undefined;}}
